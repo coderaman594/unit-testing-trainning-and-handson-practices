5ef6239eaa9fffff4bc83b7d20f093e3ce3780bb
Aman Suryawanshi <suryawanshi.aman1994@gmail.com>
2022-06-13 16:46:48 +0530

    feat:existing testcase testing

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -1,17 +1,16 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import Sinon from 'sinon';
-import {stub} from 'sinon';
+import { stub } from 'sinon';
 import * as LoanApplication from '../src/LoanApplication.js';
 import '../loan-application.js';
 
 describe('LoanApplication', () => {
   // Write test cases inside this block
 
-  it('should test the increment',async()=>{
-    const el = await fixture(html`  <LoanApplication></LoanApplication>`);
-  const des=Sinon.spy(el,'_increment').returns(true);
-  //LoanApplication._increment();
-  expect(des.calledOnce).to.be.true;
+  it('should test the increment', async () => {
+    const el = await fixture(html` <LoanApplication></LoanApplication>`);
+    const des = Sinon.spy(el, '_increment').returns(true);
+    //LoanApplication._increment();
+    expect(des.calledOnce).to.be.true;
   });
-  
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -7,51 +7,53 @@ import Sinon from 'sinon';
 
 describe('Loan EMI details', () => {
   // Write test cases inside this block
+  beforeEach(function () {
+    localStorage.setItem(
+      'emi',
+      '{"interestRate":"7.00","monthlyEMI":"447.73","principal":"10000.00","interest":"745.42","totalAmount":"10745.42"}'
+    );
+  });
+  afterEach(function () {
+    localStorage.removeItem('emi');
+  });
 
-
-  
-it('checks for method',async ()=>{
-  const el= await fixture(html `<loanemi-details></loanemi-details>`);
-  const myspy=Sinon.spy(el,'_toBasicDetails')
-  const myval=el.shadowRoot.querySelector('lion-button')[0]
-  myval.click();
-  expect(myspy.called).to.be.true;
+  it('checks for method', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    const myspy = Sinon.spy(el, '_toBasicDetails');
+    const myval = el.shadowRoot.querySelector('lion-button')[0];
+    myval.click();
+    expect(myspy.called).to.be.true;
   });
   // it('should navigate to customer page is working ',async()=>{
   //   const el = await fixture(html` <loanemi-details></loanemi-details>`);
   //  await el.shadowRoot.querySelector('.continue-btn').click();
   // });
-  it('checks all the methods',async()=>{
-    const el=await fixture(html`<loanemi-details></loanemi-details>`);
-    const fun=await Sinon.spy(el,'super');
+  it('checks all the methods', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    const fun = await Sinon.spy(el, 'super');
     await el.constructor();
     await expect(fun.calledOnce).to.be.false;
   });
 
-  it('checks for the accessibility',async()=>{
-    const el=await fixture(html`<loanemi-details></loanemi-details>`);
+  it('checks for the accessibility', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
     expect(el).to.be.accessible();
-    //document.querySelector('.emi-details').should.contain.html('<p>Interest Rate</p>');  
+    //document.querySelector('.emi-details').should.contain.html('<p>Interest Rate</p>');
   });
 
-  
-  it('checks for button click for navigation to next ',async()=>{
-    const el=await fixture(html`<loanemi-details></loanemi-details>`);
-    let btn=el.shadowRoot.querySelector('lion-button');
-    const myspy=Sinon.spy(el,'_toCustomer');
+  it('checks for button click for navigation to next ', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    let btn = el.shadowRoot.querySelector('lion-button');
+    const myspy = Sinon.spy(el, '_toCustomer');
     btn[1].click();
-    //document.querySelector('.emi-details').should.contain.html('<p>Interest Rate</p>');  
+    //document.querySelector('.emi-details').should.contain.html('<p>Interest Rate</p>');
   });
 
-  
-  
-  it('checks for button click for navigation-back ',async()=>{
-    const el=await fixture(html`<loanemi-details></loanemi-details>`);
-    let btn1=el.shadowRoot.querySelector('lion-button');
-    const myspy=Sinon.spy(el,'_toBasicDetails');
+  it('checks for button click for navigation-back ', async () => {
+    const el = await fixture(html`<loanemi-details></loanemi-details>`);
+    let btn1 = el.shadowRoot.querySelector('lion-button');
+    const myspy = Sinon.spy(el, '_toBasicDetails');
     btn1[0].click();
-    //document.querySelector('.emi-details').should.contain.html('<p>Interest Rate</p>');  
+    //document.querySelector('.emi-details').should.contain.html('<p>Interest Rate</p>');
   });
-  
-  
 });
